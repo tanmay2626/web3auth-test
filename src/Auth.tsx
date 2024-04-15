53c78cf137b1e1a450974815a093f7520d24dcff
tanmay2626 <tanmaywaykar17@gmail.com>
2024-04-15 21:29:57 +0530

    fix popup

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -59,7 +59,7 @@ const web3auth = new Web3AuthNoModal({
 // setup adapters
 const openloginAdapter = new OpenloginAdapter({
   adapterSettings: {
-    uxMode: "redirect",
+    uxMode: "popup",
   },
 });
 web3auth.configureAdapter(openloginAdapter);
