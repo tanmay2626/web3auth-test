506cf688926f623477b141496bf948b7fab4b115
tanmay2626 <tanmaywaykar17@gmail.com>
2024-04-16 12:41:58 +0530

    fix: updat telegram url

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
   const referralCode = "AXIB";
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "https://telegram.org/js/telegram-widget.js?22";
+    script.src = "7189523771:AAEoK67HKH1UjZWQM3sTfTSCoXgbcF05_0o";
     script.async = true;
     script.setAttribute("data-telegram-login", "mappatest_userBot");
     script.setAttribute("data-size", "large");
