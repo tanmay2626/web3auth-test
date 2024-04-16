8ed5b1533bafddfbacea9091e414a8c9e12d36d3
tanmay2626 <tanmaywaykar17@gmail.com>
2024-04-16 23:41:02 +0530

    fix: integration

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from "react";
+import { getGameStats, updateGameStats } from "./api";
 
 export const Game = () => {
   const [prevPoints, setPrevPoints] = useState(0);
   const [prevTaps, setPrevTaps] = useState(0);
   const [points, setPoints] = useState(0);
   const [energy, setEnergy] = useState(100);
-  const [lastActiveTime, setLastActiveTime] = useState(Date.now());
   const [taps, setTaps] = useState(0);
-  const [currRechargeLvl, setCurrRechargeLvl] = useState(0);
-  const [currTurboLvl, setCurrTurboLvl] = useState(0);
+  const [timeoutId, setTimeoutId] = useState(null); // State to keep the timeout reference
+  const [gameData, setGameData] = useState({
+    rank: 1,
+    league: 1,
+    currRechargeLvl: 1,
+    currTurboLvl: 1,
+  });
 
   useEffect(() => {
     if (energy < 100) {
       const interval = setInterval(() => {
-        setEnergy((currentEnergy) => {
-          const updatedEnergy = currentEnergy + 1;
-          return updatedEnergy > 100 ? 100 : updatedEnergy;
-        });
+        setEnergy((currentEnergy) =>
+          currentEnergy < 100 ? currentEnergy + 1 : 100
+        );
       }, 1000);
-
       return () => clearInterval(interval);
     }
   }, [energy]);
@@ -26,59 +29,64 @@ export const Game = () => {
   const handleTap = () => {
     if (energy > 0) {
       setPoints((prev) => prev + 1);
-      const newEnergy = Math.max(0, energy - 1);
-      setEnergy(newEnergy);
+      setEnergy((prev) => Math.max(0, prev - 1));
       setTaps((prev) => prev + 1);
+
+      // Clear existing timeout and set a new one
+      clearTimeout(timeoutId);
+      const newTimeoutId = setTimeout(() => {
+        sendGameStats();
+      }, 2000); // Adjust the time as necessary for your game's design
+      setTimeoutId(newTimeoutId);
     }
-    setLastActiveTime(Date.now());
+  };
+
+  const sendGameStats = async () => {
+    console.log("hello");
+
+    const updatedGameStats = {
+      coins: points,
+      numberOfTaps: taps,
+      energyBoosterLevel: energy,
+    };
+    try {
+      const response = await updateGameStats(updatedGameStats);
+      console.log(response);
+    } catch (error) {
+      console.error("Error: ", error);
+    }
   };
 
   useEffect(() => {
-    const storedPoints = parseInt(localStorage.getItem("points")) || 0;
-    const storedEnergy = parseInt(localStorage.getItem("energy")) || 100;
-    const storedTaps = parseInt(localStorage.getItem("taps")) || 100;
-    const storedLastActive =
-      parseInt(localStorage.getItem("lastActive")) || Date.now();
-    const timeNow = Date.now();
-    const secondsSinceLast = Math.floor((timeNow - storedLastActive) / 1000);
-    const recoveredEnergy = Math.min(
-      100,
-      storedEnergy + Math.min(secondsSinceLast, 100 - storedEnergy)
-    );
-    const storedRecharge =
-      parseInt(localStorage.getItem("rechargeLevel")) || 100;
-    const storedTurbo = parseInt(localStorage.getItem("turboLevel")) || 100;
-    setCurrRechargeLvl(storedRecharge);
-    setCurrTurboLvl(storedTurbo);
-    setPrevPoints(storedPoints);
-    setEnergy(Math.max(0, recoveredEnergy));
-    setLastActiveTime(storedLastActive);
-    setPrevTaps(storedTaps);
-  }, []);
+    const fetchGameStats = async () => {
+      try {
+        const response = await getGameStats();
+        console.log(response);
+        const timeNow = Date.now();
+        const timestamp = response.lastActivityTime;
+        const date = new Date(timestamp);
+        const secondsSinceLast = Math.floor((timeNow - date.getTime()) / 1000);
+        const recoveredEnergy = Math.min(
+          100,
+          response.currentEnergyLevel +
+            Math.min(secondsSinceLast, 100 - response.currentEnergyLevel)
+        );
 
-  useEffect(() => {
-    const handleUnload = () => {
-      console.log("I was called");
-
-      localStorage.setItem("points", prevPoints + points);
-      localStorage.setItem("energy", energy);
-      localStorage.setItem("taps", prevTaps + taps);
-      localStorage.setItem("lastActive", Date.now());
-      localStorage.setItem("rechargeLevel", "1");
-      localStorage.setItem("turboLevel", "1");
+        setPrevPoints(response.coins);
+        setPrevTaps(response.numberOfTaps);
+        setEnergy(recoveredEnergy);
+        setGameData({
+          rank: response.game,
+          league: response.league,
+          currRechargeLvl: response.energyBoosterLevel,
+          currTurboLvl: response.turboBoosterLevel,
+        });
+      } catch (error) {
+        console.error("Error: ", error);
+      }
     };
-
-    window.addEventListener("beforeunload", handleUnload);
-    return () => window.removeEventListener("beforeunload", handleUnload);
-  }, [
-    prevPoints,
-    points,
-    energy,
-    taps,
-    prevTaps,
-    currRechargeLvl,
-    currTurboLvl,
-  ]);
+    fetchGameStats();
+  }, []);
 
   return (
     <div>
@@ -88,9 +96,9 @@ export const Game = () => {
       </button>
       <p>Points: {prevPoints + points}</p>
       <p>Energy: {energy}</p>
-      <p>TurboLvl: {currTurboLvl}</p>
       <p>Taps: {prevTaps + taps}</p>
-      <p>RechargeLvl: {currRechargeLvl}</p>
+      <p>RechargeBoosterLvl: {gameData.currRechargeLvl}</p>
+      <p>TurboBoosterLvl: {gameData.currTurboLvl}</p>
     </div>
   );
 };
---
+++ src/api.js
@@ -0,0 +1,296 @@
+import axios from "axios";
+
+const accessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjFlYmVhZTY3MzI0YzM1Y2EzODQ3N2EiLCJpYXQiOjE3MTMyOTA5MzMsImV4cCI6MTcxODQ3NDkzM30.aIpQaLVRL0sd2VL8Gl2cgTG7aD1A45Yz0csAk18rYl0";
+
+const authenticate = async (userData, referralCode) => {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/login?referralCode=${referralCode}`,
+      {
+        publicAddress: "0xx",
+        email: "tanmay@gmail.com",
+        telegramId: 2134334,
+        telegramUsername: "twaykar",
+        telegramIsPremium: true,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    throw error;
+  }
+};
+
+// response : token, status (new user)
+
+const getProfile = async () => {
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_URL}/profile`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    throw error;
+  }
+};
+
+// response : for profile section
+
+const logout = async (accessToken) => {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/profile`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    throw error;
+  }
+};
+
+// in profile
+
+// accessToken
+const getAllTasks = async () => {
+  try {
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}/tasks`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    throw error;
+  }
+};
+
+// for tasks closed view
+
+const getTaskById = async (taskId) => {
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_URL}/tasks/${taskId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    throw error;
+  }
+};
+
+// task open view
+
+const verifyTask = async (taskId, accessToken) => {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/tasks/verify/:userId/:taskId`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    throw error;
+  }
+};
+
+// verify task button - success: true/false
+
+const claimReward = async (taskId) => {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/tasks/claim/${taskId}`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    throw error;
+  }
+};
+
+// coins and message
+
+const connectTwitter = async (twitterDetails, accessToken) => {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/connect/twitter`,
+      {
+        twitterId: 328739,
+        titterUsername: "twaykar8",
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    throw error;
+  }
+};
+
+// verify task button - success: true/false
+
+const getLeaderboard = async (level) => {
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_URL}/leaderboard?level=${level}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    throw error;
+  }
+};
+
+// array with { accountID, username, coins }
+
+const getUserStats = async () => {
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_URL}/userStats`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    throw error;
+  }
+};
+
+// For landing page : coins, rank. directRefferal, IndirectRefferals, totalUsers,currentLeague,
+
+const getAccountRaffleInfo = async () => {
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_URL}/raffleInfo`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    throw error;
+  }
+};
+
+// Dino and Tasks section - coins, rank, doneTasks[ ], highestCard, drawnCards[ ]
+// And tasks section to filter
+
+const getGameStats = async () => {
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_URL}/gameStats`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    throw error;
+  }
+};
+
+const updateGameStats = async (gameData) => {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/updategamestats`,
+      gameData,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    throw error;
+  }
+};
+
+const claimBooster = async () => {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/claimbooster`,
+      {
+        price: 750,
+        boosterType: "turbo",
+        increment: 2,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    throw error;
+  }
+};
+
+export {
+  authenticate,
+  getProfile,
+  logout,
+  getAllTasks,
+  getTaskById,
+  verifyTask,
+  claimReward,
+  connectTwitter,
+  getLeaderboard,
+  getUserStats,
+  getAccountRaffleInfo,
+  getGameStats,
+  updateGameStats,
+  claimBooster,
+};
+
+// 11 routes - non game
