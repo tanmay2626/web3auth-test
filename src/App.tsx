78cc83b6846b54856758ceb9bf9a44b763bea89f
tanmay2626 <tanmaywaykar17@gmail.com>
2024-04-16 12:23:50 +0530

    add telegram script

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from "react";
-import Auth from "./Auth";
-import Modal from "./Modal";
+import React, { useEffect } from "react";
 
 const App = () => {
-  return (
-    <div className="App">
-      <Auth />
-    </div>
-  );
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://telegram.org/js/telegram-widget.js?22";
+    script.async = true;
+    script.setAttribute("data-telegram-login", "mappatest_userBot");
+    script.setAttribute("data-size", "large");
+    script.setAttribute("data-radius", "5");
+    script.setAttribute("data-auth-url", "https://t.me/mappatest_userBot/Test");
+    script.setAttribute("data-request-access", "write");
+
+    document.body.appendChild(script);
+
+    // Cleanup the script when component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return <div className="App">Hello</div>;
 };
 
 export default App;
