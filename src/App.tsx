b74a1bfdd7c2e0eb72296f2a09892c5d5b40b7ef
tanmay2626 <tanmaywaykar17@gmail.com>
2024-04-19 13:19:09 +0530

    fix: telegram callack

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
+import TelegramLogin from "./Telegram";
 
 const App = () => {
-  const referralCode = "AXIB";
-  useEffect(() => {
-    // Define the callback function on the global scope
-    window.onTelegramAuth = function (user) {
-      alert(
-        "Logged in as " +
-          user.first_name +
-          " " +
-          user.last_name +
-          " (" +
-          user.id +
-          (user.username ? ", @" + user.username : "") +
-          ")"
-      );
-      // Add any additional logic you need here
-    };
-
-    const script = document.createElement("script");
-    script.src = "https://telegram.org/js/telegram-widget.js?7";
-    script.async = true;
-    script.setAttribute("data-telegram-login", "mappatest_userBot");
-    script.setAttribute("data-size", "large");
-    script.setAttribute("data-radius", "5");
-    script.setAttribute("data-userpic", "false");
-    script.setAttribute(
-      "data-auth-url",
-      `https://t.me/mappatest_userBot/App?startapp=${referralCode}`
-    );
-    script.setAttribute("data-request-access", "write");
-
-    document.body.appendChild(script);
-
-    // Cleanup the script and callback when component unmounts
-    return () => {
-      delete window?.onTelegramAuth; // Remove the callback from the global scope
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  return <div className="App">Hello</div>;
+  return (
+    <div>
+      <TelegramLogin />
+    </div>
+  );
 };
 
 export default App;
--- src/Telegram.jsx
+++ src/Telegram.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-export const Telegram = () => {
-    <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="samplebot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
-<script type="text/javascript">
-  function onTelegramAuth(user) {
-    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-  }
-</script>
+const TelegramLogin = () => {
+  useEffect(() => {
+    // Load Telegram JavaScript SDK asynchronously
+    const script = document.createElement("script");
+    script.src = "https://telegram.org/js/telegram-widget.js?22";
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Define the onTelegramAuth function
+    window.onTelegramAuth = (user) => {
+      alert(
+        "Logged in as " +
+          user.first_name +
+          " " +
+          user.last_name +
+          " (" +
+          user.id +
+          (user.username ? ", @" + user.username : "") +
+          ")"
+      );
+    };
+
+    return () => {
+      // Clean up: Remove the script when component unmounts
+      document.body.removeChild(script);
+      delete window.onTelegramAuth;
+    };
+  }, []);
+
   return (
-    <div>Telegram</div>
-  )
-}
+    <div>
+      <script
+        async
+        src="https://telegram.org/js/telegram-widget.js?22"
+        data-telegram-login="mappatest_userBot"
+        data-size="large"
+        data-onauth="onTelegramAuth(user)"
+        data-request-access="write"
+      ></script>
+    </div>
+  );
+};
+
+export default TelegramLogin;
