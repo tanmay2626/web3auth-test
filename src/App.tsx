d825552b20232cbcea9d2fa1aba9e79b49fe723b
tanmay2626 <tanmaywaykar17@gmail.com>
2024-04-20 15:24:18 +0530

    feat: update telegram mini app script

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TelegramLogin from "./Telegram";
 
+// Declare the Telegram property on the Window object
+declare global {
+  interface Window {
+    Telegram: any; // Adjust the type according to the structure of the Telegram object
+  }
+}
+
+const tele = window.Telegram?.WebApp;
 const App = () => {
+  const [user, setUser] = useState<string | null>(null); // Specify the type of user state
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        if (tele) {
+          await tele.ready();
+          setUser(JSON.stringify(tele.initDataUnsafe.user));
+        } else {
+          console.error("Telegram WebApp API not available");
+        }
+      } catch (error) {
+        console.error("Error fetching user data from Telegram:", error);
+      }
+    };
+    getUserData();
+  }, [user]);
+
   return (
     <div>
-      <TelegramLogin />
+      <p>Hello</p>
+      {user && <p>{user}</p>}
     </div>
   );
 };
