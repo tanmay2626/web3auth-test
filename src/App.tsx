9e8b26ceb875f087ccb8d93b0ae592826f67fbcb
tanmay2626 <tanmaywaykar17@gmail.com>
2024-04-24 13:25:33 +0530

    fix: update get start app code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import TelegramLogin from "./Telegram";
+import { Twitter } from "./Twitter";
 
 // Declare the Telegram property on the Window object
 declare global {
@@ -11,13 +12,14 @@ declare global {
 const tele = window.Telegram?.WebApp;
 const App = () => {
   const [user, setUser] = useState<string | null>(null); // Specify the type of user state
-
+  const [refer, setRefer] = useState<string | null>(null);
   useEffect(() => {
     const getUserData = async () => {
       try {
         if (tele) {
           await tele.ready();
           setUser(JSON.stringify(tele.initDataUnsafe.user));
+          setRefer(JSON.stringify(tele.initDataUnsafe.start_param));
         } else {
           console.error("Telegram WebApp API not available");
         }
@@ -32,6 +34,7 @@ const App = () => {
     <div>
       <p>Hello</p>
       {user && <p>{user}</p>}
+      {refer && <p>{refer}</p>}
     </div>
   );
 };
--- src/Twitter.jsx
+++ src/Twitter.jsx
@@ -1,8 +1,59 @@
-import React from "react";
-import { TwitterAuthProvider } from "firebase/auth";
+import React, { useEffect } from "react";
+import { initializeApp } from "firebase/app"; // Importing Firebase initialization function
+import {
+  TwitterAuthProvider,
+  getAuth,
+  signInWithRedirect,
+  getRedirectResult,
+} from "firebase/auth"; // Importing necessary Firebase Auth services
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJ1mP-1RLsDuW2Falds6dv8-xn6Tz0F58",
+  authDomain: "twitter-test-928f2.firebaseapp.com",
+  projectId: "twitter-test-928f2",
+  storageBucket: "twitter-test-928f2.appspot.com",
+  messagingSenderId: "991790305360",
+  appId: "1:991790305360:web:9366f1e1961746925e4155",
+  measurementId: "G-DZY3QVS505",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 export const Twitter = () => {
   const provider = new TwitterAuthProvider();
 
-  return <div>Twitter</div>;
+  const handleLogin = () => {
+    signInWithRedirect(auth, provider);
+  };
+
+  useEffect(() => {
+    getRedirectResult(auth)
+      .then((result) => {
+        if (result) {
+          // This gives you the Twitter OAuth 1.0 Access Token and Secret.
+          // You can use these server side with your app's credentials to access the Twitter API.
+          const credential = TwitterAuthProvider.credentialFromResult(result);
+          const token = credential.accessToken;
+          const secret = credential.secret;
+
+          // The signed-in user info.
+          const user = result.user;
+          console.log(user);
+          // Additional IdP data can be accessed if needed
+          // const additionalUserInfo = getAdditionalUserInfo(result);
+        }
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        console.error("Authentication error:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <button onClick={handleLogin}>Connect with Twitter</button>
+    </div>
+  );
 };
