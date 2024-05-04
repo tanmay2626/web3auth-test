cc2ae1e057df50fe2d6688b82abcec9f6a968a59
tanmay2626 <tanmaywaykar17@gmail.com>
2024-05-04 23:03:14 +0530

    test: is premium

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,16 @@ const tele = window.Telegram?.WebApp;
 const App = () => {
   const [user, setUser] = useState<object | null>(null); // Specify the type of user state
   const [refer, setRefer] = useState<string | null>(null);
+  const [prem, setPrem] = useState<string | null>(null);
+
   useEffect(() => {
     const getUserData = async () => {
       try {
         if (tele) {
           await tele.ready();
-          setUser(tele.initDataUnsafe.user);
+          setUser(tele.initDataUnsafe.user.is_premium);
           setRefer(tele.initDataUnsafe.is_premium);
+          setPrem(tele.is_premium);
         } else {
           console.error("Telegram WebApp API not available");
         }
@@ -28,12 +31,14 @@ const App = () => {
       }
     };
     getUserData();
-  }, [user]);
+  }, [user, refer, prem]);
 
   return (
     <div>
       <p>Hello</p>
       <p>{refer ? JSON.stringify(refer) : "NA"}</p>
+      <p>{user ? JSON.stringify(user) : "NA"}</p>
+      <p>{prem ? JSON.stringify(prem) : "NA"}</p>
     </div>
   );
 };
